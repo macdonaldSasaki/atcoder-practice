b26d3708f28412ff5fccf6762cdc3ed32549fb08
macdonaldSasaki <gogigaga11o1@outlook.jp>
2025-12-23 01:22:42 +0900

    A04

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,13 @@
 
+
 fun main() {
-    val goods = listOf<Int>(300, 300, 400, 900)
-    val sum = 1000
-    var found = false
+    var n = 13
+    var bin = ""
+    while ( n > 0){
+        bin = (n % 2).toString()+ bin
+        n /= 2
 
-    for (i in 0 until goods.size) {
-        for (j in i + 1 until goods.size) {
-            for (k in j + 1 until goods.size) {
-                if (goods[i] + goods[j] + goods[k] == sum) {
-                    found = true
-                }
-            }
-        }
-    }
-    if (found) println("yes") else println("no")
-}
 
-class GoodsChecker(private val goods: List<Int>) {
-    fun canMakeSum(target: Int): Boolean {
-        for (i in 0 until goods.size) {
-            for (j in i + 1 until goods.size) {
-                for (k in j + 1 until goods.size) {
-                    if (goods[i] + goods[j] + goods[k] == target) {
-                        return true
-                    }
-                }
-            }
-        }
-        return false
     }
+    println(bin.padStart(10,'0'))
 }
