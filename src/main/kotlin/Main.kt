c396a154e05241741ad9200dc770ac0a793bc3dc
macdonaldSasaki <gogigaga11o1@outlook.jp>
2025-12-23 01:09:41 +0900

    Solved A03

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,15 @@
 
 fun main() {
-    val (n, k) = readLine()!!.split(" ").map { it.toInt() }
+    val (n, m, k) = readLine()!!.split(" ").map { it.toInt() }
     val a = readLine()!!.split(" ").map { it.toInt() }
+    val b = readLine()!!.split(" ").map { it.toInt() }
 
-    for (i in 0 until n) {
-        if (a[i] == k) {
-            println("Yes")
-            return
+    for (i in 0 until n) {          // A のインデックスを回す
+        for (j in 0 until m) {      // B のインデックスを回す
+            if (a[i] + b[j] == k) { // A[i] と B[j] の和が K か？
+                println("Yes")
+                return
+            }
         }
     }
 
