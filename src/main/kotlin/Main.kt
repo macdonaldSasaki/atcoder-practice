4c6ce3283ad82c215feaf471c4696fb3e3c4b1f9
macdonaldSasaki <gogigaga11o1@outlook.jp>
2025-12-23 00:46:54 +0900

    Solved A02

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 
 fun main() {
-    val n = readLine()!!.toInt()
-    println(n * n)
+    val (n, k) = readLine()!!.split(" ").map { it.toInt() }
+    val a = readLine()!!.split(" ").map { it.toInt() }
+
+    for (i in 0 until n) {
+        if (a[i] == k) {
+            println("Yes")
+            return
+        }
+    }
+
+    println("No")
 }
