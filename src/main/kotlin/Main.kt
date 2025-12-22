07ea6520aef1de7c1a32491c1cadc2bc84dce517
macdonaldSasaki <gogigaga11o1@outlook.jp>
2025-12-23 01:30:00 +0900

    A06

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,22 @@
 
 fun main() {
-    val n = listOf<Int>(1, 2, 3)
-    val k = 6
-    var count = 0
+    val n  = listOf(8,6,9,1,2,1,10,100,1000,10000)
+    val a  = 10
+    val queries = listOf(
+        2 to 3,
+        1 to 4,
+        3 to 9,
+        6 to 8,
+        1 to 10
+    )
 
-    for (x in n) {
-        for (y in n) {
-            val z = k - x - y
-            if (k == x + y + z && n.contains(z)) {
-                count++
+    val s = IntArray(a + 1)
+    for(i in 0 until a){
+        s[i + 1]=s[i] + n[i]
 
-            }
-        }
     }
+    for((l,r) in queries){
+        println(s[r] - s[l - 1])
+    }
+
 }
