37fdf3335c4b30990c0abce419efd72369b7327a
macdonaldSasaki <gogigaga11o1@outlook.jp>
2025-12-23 13:20:55 +0900

    B07

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,19 @@
 
 fun main() {
-    val d = readLine()!!.toInt()
-    val n  = readLine()!!.toInt()
-    val xy = List(n){ readLine()!!.split("").map { it.toInt() }}
+    val t = readLine()!!.toInt()
+    val  n = readLine()!!.toInt()
+    val xy = List(n){ readLine()!!.split(" ").map { it.toInt() }}
 
-    val b = IntArray(d + 2)
-    for ((l,r) in xy){
+    val b = IntArray(t + 1)
+    for ((l,r) in xy ){
         b[l] += 1
-        b[r + 1] -= 1
+        b[r] -= 1
     }
 
-    var current = 0
-    for(i in 1..d){
-        current += b[i]
-        println(current)
+    var now = 0
+    for (i in 0 until t){
+        now = b[i] + now
+        println(now)
     }
-
-
 
 }
