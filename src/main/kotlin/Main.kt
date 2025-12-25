7d0299e4d7653e6626652713e1e82a2ff42382c6
macdonaldSasaki <gogigaga11o1@outlook.jp>
2025-12-25 20:33:08 +0900

    A13

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,17 @@
 
 fun main() {
-    val (n,k) = readLine()!!.split(" ").map { it.toInt() }
-    val a = readLine()!!.split(" ").map { it.toLong() }
+   val (n,k) = readLine()!!.split(" ").map { it.toInt() }
+   val a = readLine()!!.split(" ").map { it.toInt() }
 
-    var l = 1L
-    var r = 1_000_000_000L
-    var ans = r
+   var r = 0
+   var ans = 0L
 
-    while (l <= r) {
-        val mid = (l + r) / 2
-        var total = 0L
-        for(i in a){
-            total += mid / i
-            if(total >= k) break
-        }
-        if(total >= k){
-            ans = mid
-            r = mid - 1
-        }else{
-           l = mid + 1
-        }
-
-
-    }
+   for(l in 0 until n){
+       while (r < n - 1 && a[r + 1] - a[l] <= k){
+           r++
+       }
+       ans += r - l
+   }
     println(ans)
 }
 
