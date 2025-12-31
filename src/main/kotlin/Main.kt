fac426e868f00f7bbc4dd9d0e649e87a14160666
macdonaldSasaki <gogigaga1101@outlook.jp>
2026-01-01 00:25:35 +0900

    A29

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,26 +3,18 @@
 import kotlin.math.max
 
 fun main() {
-  val n = readLine()!!.toInt()
-    var current = 0
+  val input = readLine()!!.split("")
+   var x = input[0].toLong()
+   var y =input[1].toLong()
+    val m = 1000000007L
+    var ans = 1L
 
-    repeat(n){
-   val line = readLine()!!.split("")
-    val t = line[0]
-    val a = line[1].toInt()
-
-      if(t == "+"){
-        current += a
-      }else if(t == "-"){
-        current -= a
-      }else if(t == "*"){
-        current *= a
-      }
-      current %= 10000
-      if(current < 0){
-        current += 10000
-      }
-      println(current)
+    while (y>0){
+        if((y and 1L) == 1L){
+            ans = (ans * x) % m
+        }
+        x = (x*x) % m
+        y = y shr 1
     }
-
+    println(ans)
 }
