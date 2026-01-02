7d8380c76e1f29dd7411ef7587604487bb198ad9
macdonaldSasaki <gogigaga1101@outlook.jp>
2026-01-02 17:10:22 +0900

    A31

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,40 +2,13 @@
 
 import kotlin.math.max
 
-fun main() {
-    val input = readLine()!!.split(" ")
-    val n = input[0].toLong()
-    val r = input[1].toLong()
-    val m = 1000000007L
+fun main(){
+ val n = readLine()!!.toLong()
+    val a = n / 3
+    val b = n / 5
+    val c = n / 15
 
-    var numerator = 1L
-    for (i in 1..n) {
-        numerator = (numerator * i) % m
-    }
+    val ans = a + b - c
+    println(ans)
 
-    var denominator = 1L
-    for (i in 1..r) {
-        denominator = (denominator * i) % m
-    }
-    for (i in 1..(n - r)) {
-        denominator = (denominator * i) % m
-    }
-
-    val inverse = power(denominator, m - 2, m)
-
-    println((numerator * inverse) % m)
-}
-
-fun power(base: Long, exponent: Long, mod: Long): Long {
-    var p = base
-    var q = exponent
-    var ans = 1L
-    while (q > 0) {
-        if ((q and 1L) == 1L) {
-            ans = (ans * p) % mod
-        }
-        p = (p * p) % mod
-        q = q shr 1
-    }
-    return ans
 }
