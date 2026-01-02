eaf4ce2a312b8327beb8d8db85f7dd65ee1177b0
macdonaldSasaki <gogigaga1101@outlook.jp>
2026-01-02 22:40:52 +0900

    A36

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,11 +3,10 @@
 import kotlin.math.max
 
 fun main() {
-    val n = readLine()!!.toLong()
-    val a = n / 3
-    val b = n / 5
-    val c = n / 15
-
-    val ans = a + b - c
-    println(ans)
+   val (n,k) = readLine()!!.split(" ").map { it.toLong() }
+    if(k >= 2 * n - 2 && k % 2 == 0L){
+        println("Yes")
+    }else{
+        println("No")
+    }
 }
