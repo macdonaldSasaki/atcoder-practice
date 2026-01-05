27bd25921861386b24120fd0924abde9e5fcdf16
macdonaldSasaki <gogigaga1101@outlook.jp>
2026-01-05 15:35:33 +0900

    350B

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,19 @@
 
 
 fun main() {
-val n = readLine()!!
-    val x = n.filter {it.isDigit() }.toInt()
-    if(x >= 1 && x <= 349 && x != 316){
-        println("Yes")
-    }else{
-        println("No")
+val n = readLine()!!.split(" ").map { it.toInt() }
+    val x = readLine()!!.split(" ").map { it.toInt() }
+    var y = n[0]
+    val z = mutableListOf<Int>()
+    for(i in x) {
+        if (!z.contains(i)) {
+            y -= 1
+            z.add(i)
+        }else{
+            y += 1
+            z.remove(i)
+        }
     }
-
+    println(y)
 }
 
