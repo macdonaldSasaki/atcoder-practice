4baf230b8747d3e40eb5ae93a61a902b9acb99e0
macdonaldSasaki <gogigaga1101@outlook.jp>
2026-01-11 14:50:18 +0900

    400A

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,9 @@
 
 
 fun main() {
-val n = readLine()!!.split(" ").map { it.toInt() }
-    val x = readLine()!!.split(" ").map { it.toInt() }
-    var y = n[0]
-    val z = mutableListOf<Int>()
-    for(i in x) {
-        if (!z.contains(i)) {
-            y -= 1
-            z.add(i)
-        }else{
-            y += 1
-            z.remove(i)
-        }
-    }
-    println(y)
+    val (n,k) = readLine()!!.split(" ").map { it.toInt() }
+    val x = n *(1 shl k)
+    println(x)
 }
 
+git add . git commit -m "A"  git push
