92d7f683d72dc65cb8d0158a8d519a9d42e39dca
macdonaldSasaki <gogigaga1101@outlook.jp>
2026-01-13 20:31:33 +0900

    440C 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,57 +1,38 @@
 
 
 fun main() {
-    val tStr = readLine()
-    if (tStr == null) return
-    val t = tStr.toInt()
+    val T = readLine()!!.toInt()
+    repeat(T) {
+        val (N, W) = readLine()!!.split(" ").map { it.toInt() }
+        val C = readLine()!!.split(" ").map { it.toLong() }
 
-    // 1. テストケースの数だけ repeat
-    repeat(t) {
-        val line2 = readLine()!!
-        val split2 = line2.split(" ")
-        val n = split2[0].toInt()
-        val w = split2[1].toLong()
-        val m = (2 * w).toInt()
-
-        val line3 = readLine()!!
-        val split3 = line3.split(" ")
-
-        // いもす法用の配列（サイズは m+1 必要）
-        val diff = LongArray(m + 1)
-
-        for (i in 0 until n) {
-            val cost = split3[i].toLong()
-            val maskId = i + 1
-
-            // 開始位置 l と 終了位置 r を計算
-            val l = ((-maskId % m + m) % m).toInt()
-            val r = ((l + w) % m).toInt()
-
-            // 2. ここが「いもす法」の核心（repeatせず、+と-だけで済ます）
-            if (l < r) {
-                // またがない時
-                diff[l] += cost
-                diff[r] -= cost
-            } else {
-                // またぐ時（右端と左端に分ける）
-                diff[l] += cost
-                diff[m] -= cost // 右端まで
-                diff[0] += cost // 左端から
-                diff[r] -= cost
-            }
+        // N <= W のときは黒を0にできる
+        if (N <= W) {
+            println(0)
+            return@repeat
         }
 
-        // 3. 累積和（ここで一気に塗りつぶす）
-        for (j in 0 until m) {
-            diff[j+1] += diff[j]
+        // 配列を2倍にして円環を直線化
+        val A = LongArray(2 * N)
+        for (i in 0 until N) {
+            A[i] = C[i]
+            A[i + N] = C[i]
         }
 
-        var minVal = Long.MAX_VALUE
-        for (j in 0 until m) {
-            if (diff[j] < minVal) {
-                minVal = diff[j]
-            }
+        // 累積和
+        val pref = LongArray(2 * N + 1)
+        for (i in 0 until 2 * N) {
+            pref[i + 1] = pref[i] + A[i]
         }
-        println(minVal)
+
+        // 長さWの連続区間（黒ブロック）を全探索
+        var ans = Long.MAX_VALUE
+        for (l in 0 until N) {
+            val r = l + W
+            val cost = pref[r] - pref[l]
+            if (cost < ans) ans = cost
+        }
+
+        println(ans)
     }
 }
