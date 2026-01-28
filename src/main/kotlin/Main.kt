971d30c03913f2ea1555d2f41515257e7baae843
macdonaldSasaki <gogigaga1101@outlook.jp>
2026-01-29 08:19:34 +0900

    442C 組み合わせ

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,28 @@
 import kotlin.math.max
 
+fun main(){
+    val s = readLine()!!.split(" ")
+    val n = s[0].toInt()
+    val m = s[1].toInt()
+    val d = IntArray(n + 1)
 
-fun main() {
-    val (x, y) = readLine()!!.split(" ").map { it.toInt() }
-    val d = IntArray(x + 1)
-
-    repeat(y) {
-        val (u, v) = readLine()!!.split(" ").map { it.toInt() }
+    repeat(m){
+        val t = readLine()!!.split(" ")
+        val u = t[0].toInt()
+        val v = t[1].toInt()
         d[u]++
         d[v]++
     }
-    val r = LongArray(x)
-    for (i in 1..x) {
-        val k = (x - 1 - d[i]).toLong()
-        if (k < 3) {
+
+    val r = LongArray(n)
+    for(i in 1..n){
+        val k = (n - 1 - d[i] ).toLong()
+        if(k < 3){
             r[i - 1] = 0
-        } else {
-            val v = (0..2).fold(1L) { a, b -> a * (k - b) }
-            r[i - 1] = v / 6
+        }else{
+            r[i - 1] = k * (k - 1) * (k - 2) / 6
         }
     }
-    println(r.joinToString(" "))
+
+println(r.joinToString (" "))
 }
-
-
