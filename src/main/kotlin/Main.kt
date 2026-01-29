cff8f8e2658dea45d6310ac1fc3ca71187552259
macdonaldSasaki <gogigaga1101@outlook.jp>
2026-01-29 11:37:42 +0900

    436C 四角形の移動

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,30 @@
 import kotlin.math.max
 
 fun main() {
-    val s1 = readLine()!!.toInt()
-    repeat(s1) {
-        val n = readLine()!!.toInt()
-        var s = 0L
-        val c = LongArray(n)
-        for (i in 0 until n) {
-            val f = readLine()!!.split(" ")
-            val w = f[0].toLong()
-            val p = f[1].toLong()
-            s += p
-            c[i] = w + p
-        }
-        c.sort()
+    val s1 = readLine()!!.split(" ")
+    val n = s1[0].toLong()
+    val m = s1[1].toInt()
+    val b = n + 1
+    val h = mutableSetOf<Long>()
+    var a = 0
 
-        val k = LongArray(n + 1)
-        for(i in 0 until n) k[i + 1] = k[i] + c[i]
-        var l = 0
-        var r = n
-        var a = 0
-        while(l <= r){
-            val m = (l + r) / 2
-            if(k[m] <= s){
-                a = m
-                l = m + 1
-            }else{
-                r = m - 1
+    repeat(m) {
+        val s2 = readLine()!!.split(" ")
+        val r = s2[0].toLong()
+        val c = s2[1].toLong()
+
+        var f = false
+        for (x in -1.. 1){
+            for(y in -1 .. 1){
+                if(h.contains((r + x) * b + (c + y))){
+                    f = true
+                }
             }
         }
-        println(a)
+        if(!f){
+            h.add(r * b + c)
+            a++
+        }
     }
+    println(a)
 }
