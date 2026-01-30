5ac22c7d2b6835f9101de9ca83477720abe448c3
macdonaldSasaki <gogigaga1101@outlook.jp>
2026-01-30 17:04:23 +0900

    431Bグリッドの移動

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,21 @@
 import kotlin.math.max
 
 fun main() {
-    val s = readLine()!!
-    val n = s.length
-    if (n == 0) {
-        println(0)
-        return
-    }
+  val i = readLine()!!.split(" ").map { it.toInt() }
+    val n = i[0]
+    val m = i[1]
+    val s = Array(n){readLine()!!}
+    val u = mutableSetOf<String>()
 
-    val v = mutableListOf<Int>()
-    val l = mutableListOf<Int>()
-    var i = 0
-    while (i < n) {
-        var j = i
-        while (j < n && s[j] == s[i]) {
-            j++
-        }
-        v.add(s[i] - '0')
-        l.add(j - i)
-        i = j
-    }
+    for(r in 0 ..  n - m){
+        for(c in 0 .. n - m){
+            var t = ""
+            for(k in 0 until m){
+                t += s[r + k].substring(c , c + m) + "|"
+            }
+            u.add(t)
 
-    var a: Long = 0
-    for (k in 0 until v.size - 1) {
-        if (v[k] + 1 == v[k + 1]) {
-            val m = if (l[k] < l[k + 1]) l[k] else l[k + 1]
-            a += m.toLong()
-        }
     }
-    println(a)
+}
+    println(u.size)
 }
