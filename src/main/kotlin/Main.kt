c787de94276ec24b1488033872884fc964adccd3
macdonaldSasaki <gogigaga1101@outlook.jp>
2026-01-31 23:01:41 +0900

    433C

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,29 @@
 import kotlin.math.max
 
 fun main() {
-  val i = readLine()!!.split(" ").map { it.toInt() }
-    val n = i[0]
-    val m = i[1]
-    val s = Array(n){readLine()!!}
-    val u = mutableSetOf<String>()
-
-    for(r in 0 ..  n - m){
-        for(c in 0 .. n - m){
-            var t = ""
-            for(k in 0 until m){
-                t += s[r + k].substring(c , c + m) + "|"
+    var i = readLine()!!.split(" ")
+    var n = i[0].toInt()
+    var t = i[1].toLong()
+    var w = 0L
+    var o = 0L
+    var s = true
+    if (n > 0) {
+        var a = readLine()!!.split(" ").map { it.toLong() }
+        for (p in a) {
+            if (p >= o) {
+                s = true
+            } else {
+                s = false
             }
-            u.add(t)
-
+            if (s == true) {
+                w += (p - o)
+                o = p + 100
+                s = false
+            }
+        }
     }
-}
-    println(u.size)
+    if (o < t) {
+        w = w + (t - o)
+    }
+    println(w)
 }
