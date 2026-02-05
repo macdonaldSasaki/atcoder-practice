d929faa17a0b05aa22cd0b49cc52c18a5ee46d2f
macdonaldSasaki <gogigaga1101@outlook.jp>
2026-02-05 20:55:02 +0900

    ABC57C

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,22 @@
 import kotlin.math.abs
+import kotlin.math.*
 
 fun main() {
-  val (h, w) = readLine()!!.split(" ").map { it.toInt() }
-  val s = Array(h) { readLine()!! }
+val n = readLine()!!.toLong()
+    var min = 11
+    var i = 1L
+    while(i * i <= n){
+        if(n % i == 0L){
+            val a = i
+            val b = n / i
+            val a1 = a.toString().length
+            val b1 = b.toString().length
 
-  val dy = intArrayOf(-1,1,0,0)
-  val dx = intArrayOf(0,0,-1,1)
-
-  for(i in 0 until h){
-    for(j in 0 until w){
-
-      if(s[i][j] == '#'){
-        var ok = false
-         for(k in 0 until 4){
-           val ni = i + dy[k]
-           val nj = j + dx[k]
-
-           if(ni >= 0 && ni < h && nj >= 0 && nj < w){
-             if(s[ni][nj] == '#'){
-               ok = true
-             }
-           }
-         }
-        if(!ok){
-          println("No")
-          return
+            val f = max(a1,b1)
+            min = min(min,f)
         }
-      }
-
+        i++
     }
-  }
-  println("Yes")
+    println(min)
+
 }
